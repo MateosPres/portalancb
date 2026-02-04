30c9e17af0520a0cbb0ea5df0e6d62444af8c7c2
pres <mateospres@gmail.com>
2026-02-04 16:12:04 -0400

    Icon and adreess mobile fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/', // Alterado de './' para '/' para evitar problemas de rota no PWA
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
