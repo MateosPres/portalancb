c2b387bb353c3e5f92da4544f7d9a12c3f7c8402
pres <mateospres@gmail.com>
2026-02-04 15:37:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Define base relativa para funcionar no GitHub Pages em qualquer subdiretório
+  base: "/",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
